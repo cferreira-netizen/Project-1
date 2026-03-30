0002a1700d5fb346641f5cd287cd25e044b19d4f
cferreira-netizen <cferreira@caldwell.edu>
2026-03-30 19:40:47 +0000

    Mar 30, 2026, 3:40 PM

--- src/assets/sprites.py
+++ src/assets/sprites.py
@@ -15,3 +15,32 @@ import random
 PIPE_GREEN = (80, 200, 90)
 PIPE_DARK = (50, 160, 60)
 BIRD_YELLOW = (255, 220, 50)
+BIRD_DARK = (200, 160, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+ORANGE = (255, 160, 30)
+BLUE = (50, 120, 255)
+PURPLE = (180, 50, 220)
+
+#Power Ups
+PU_SLOW = 'slow'
+PU_SHRINK = 'shrink'
+PU_FAST = 'fast'
+
+# Physics
+GRAVITY = 0.45
+FLAP_STRENGTH = -8.5
+GROUND_Y = 560
+
+class Bird:
+    BASE_W = 38
+    BASE_H = 28
+
+def __init__(self, x, y):
+    self.x = float(x)
+    self.y = float(y)
+    self.w = self.BASE_W
+    self.h = self.BASE_H
+    self.vel_y = 0
+    self.alive = True
+    self.power_up = None
