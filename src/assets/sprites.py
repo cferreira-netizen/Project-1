8d4bd6db2b4244a925ed0ecc4bb1605029d8902b
cferreira-netizen <cferreira@caldwell.edu>
2026-03-30 19:04:20 +0000

    Mar 30, 2026, 3:04 PM

---
+++ src/assets/sprites.py
@@ -0,0 +1,15 @@
+"""
+Sprites.py
+
+all game objects classes for flappy bird + Power ups
+
+Classes:
+- Bird
+- Pipe
+- PowerUp
+"""
+import pygame
+import random
+
+# Colors
+PIPE_GREEN = (80, 200, 120)
